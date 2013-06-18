22f9468262fae6c0134eac076dd57f62c284ecba
Pete Hawkes <pete@oblong.com>
2013-06-18 12:59:50 -0700

    renamed to NOC_1_2_bouncingball_vectors_gspeak

--- Greenhouse/chp1_vectors/NOC_1_2_bouncingball_vectors_gspeak/NOC_1_2_bouncingball_vectors_gspeak.C
+++ Greenhouse/chp1_vectors/NOC_1_2_bouncingball_vectors_gspeak/NOC_1_2_bouncingball_vectors_gspeak.C
@@ -8,7 +8,7 @@
  A port to Greenhouse of examples from The Nature of Code by Daniel Shiffman
  
  Example 1-2: Bouncing Ball, with Vectors! Loc () and PhysLoc ()
- Using a Box for now to keep drawing simple.
+ Set up for g-speak Felds in space instead of directly accessing x and y coordinates. 
  
  **/
 
@@ -22,7 +22,7 @@ class Box  :  public Thing
   
   
   Box ()  :  Thing ()
-  { SlapOnFeld ();    
+  { SlapOnFeld ();
   }
   
   void Travail ()
@@ -41,18 +41,18 @@ class Box  :  public Thing
     
     float xEdge = (f -> Loc () + o * f -> Width () / 2.0).Dot(o);
     float yEdge = (f -> Loc () + u * f -> Height () / 2.0).Dot(u);
-
+    
     if (v_o > xEdge ) {
       INFORM ( "HIT xEdge" );
       velocity.x = velocity.x * -1;
     }
     
     /*if (v_u > yEdge || v_u < -yEdge) {
-      INFORM ( "HIT yEdge" );
-      velocity.y = velocity.y * -1;
-    }
-    */
-
+     INFORM ( "HIT yEdge" );
+     velocity.y = velocity.y * -1;
+     }
+     */
+    
   }
   
   void DrawSelf ()
